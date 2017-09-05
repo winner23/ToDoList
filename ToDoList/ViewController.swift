2caf6a2a7d5050154f4ba41286f74bdbff23bd1c
Volodymyr Vinyarskyy <v.winner@gmail.com>
2017-09-05 22:48:17 +0300

    Fix: Insert and delete work throughout CoreDate

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addTaskField: UITextField!
     
-    var taskList: [String] = []
+    var taskList: [TaskModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,33 +15,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBAction func addButtonTapped(_ sender: YTRoundedButton) {
-        
-        insertNewTask()
-    }
-    
-    
-    func insertNewTask() {
-        
         if addTaskField.text!.isEmpty {
             let alert = UIAlertController(title: "Worning!", message: "Task cannot be empty!", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             return
         }
-        if taskList.contains(addTaskField.text!) {
-            let alert = UIAlertController(title: "Worning!", message: "This task already exists!", preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            addTaskField.text = ""
-            return
-        }
+        insertNewTask()
+    }
+    
+    
+    func insertNewTask() {
+        
+        
+//        if taskList.contains(addTaskField.text!) {
+//            let alert = UIAlertController(title: "Worning!", message: "This task already exists!", preferredStyle: UIAlertControllerStyle.alert)
+//            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+//            self.present(alert, animated: true, completion: nil)
+//            addTaskField.text = ""
+//            return
+//        }
         
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let task = TaskModel(context: context)
         
-        task.name = addTaskField.text
-        
-        taskList.append(addTaskField.text!)
+        task.name = addTaskField.text!
+        taskList.append(task)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         let indexPath = IndexPath(row: taskList.count - 1, section: 0)
         
@@ -54,9 +54,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 }
 
-
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
+    override func viewWillAppear(_ animated: Bool) {
+        //
+        getData()
+        //
+        tableView.reloadData()
+    }
+    
+    func getData() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+        taskList = try context.fetch(TaskModel.fetchRequest())
+        } catch {
+            print("Fetching faild!")
+        }
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return taskList.count
     }
@@ -67,11 +81,10 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let taskText = taskList[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell") as! TaskCell
-        cell.taskTitle.text = taskText
+        cell.taskTitle.text = taskText.name
         
         return cell
     }
-    
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
@@ -81,22 +94,16 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return true
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete {
-            taskList.remove(at: indexPath.row)
-            
-            tableView.beginUpdates()
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-            tableView.endUpdates()
-        }
-        
-    }
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?
     {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let task = taskList[indexPath.row]
+        
         let update = UITableViewRowAction(style: .normal, title: "Update") { action, index in
-            self.addTaskField.text = self.taskList[indexPath.row]
+            self.addTaskField.text = self.taskList[indexPath.row].name
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
             
             self.taskList.remove(at: indexPath.row)
             
@@ -108,6 +115,8 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         }
         let delete = UITableViewRowAction(style: .default, title: "Delete") { action, index in
             
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
             self.taskList.remove(at: indexPath.row)
             
             tableView.beginUpdates()
